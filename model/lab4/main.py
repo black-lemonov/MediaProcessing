502ea5f4d90d5f2811a9d67ca61bdad62e42bf8a
black-lemonov <egor.porf1@mail.ru>
2024-11-29 02:21:06 +0300

    [лаб 4] добавлен скрипт для запуска детектора границ

---
+++ model/lab4/main.py
@@ -0,0 +1,27 @@
+import tkinter.filedialog as fd
+import cv2
+
+from canny import director
+
+
+def main() -> None:
+    img_path = fd.askopenfilename(
+        title='Выберите изображение:',
+        initialdir="папка с изображениями",
+        filetypes=(('',".png .jpg .jpeg .ico"),)
+    )
+    img = cv2.imread(img_path)
+    cv2.namedWindow("Исходное изображение")
+    cv2.imshow('Исходное изображение', img)
+    
+    edges = director.sobel().detect(img)
+    cv2.namedWindow("Алгоритм Канни")
+    cv2.imshow("Алгоритм Канни", edges)
+    
+    if cv2.waitKey(0) == 27:
+        cv2.destroyAllWindows()
+    
+
+
+if __name__ == "__main__":
+    main()
