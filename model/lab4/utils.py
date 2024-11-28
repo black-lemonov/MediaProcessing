303b1386cd0f1e31041851496ebcb96c7336d47d
black-lemonov <egor.porf1@mail.ru>
2024-11-28 22:23:36 +0300

    [лаб4] добавлена функция для нахождения соседей по углу

--- model/lab4/utils.py
+++ model/lab4/utils.py
@@ -16,3 +16,20 @@ def get_pixel(x, y, tg):
         return 6
     elif x <= 0 and y <= 0 and tg < 2.414:
         return 7
+    
+
+def get_neighbours(x, y, angle: int) -> tuple[tuple[int, int], tuple[int, int]]:
+    if angle == 0 or angle == 4:
+        neighbour1 = (y - 1, x)
+        neighbour2 = (y + 1, x)
+    elif angle == 1 or angle == 5:
+        neighbour1 = (y - 1, x + 1)
+        neighbour2 = (y + 1, x - 1)
+    elif angle == 2 or angle == 6:
+        neighbour1 = (y, x + 1)
+        neighbour2 = (y, x - 1)
+    elif angle == 3 or angle == 7:
+        neighbour1 = (y + 1, x + 1)
+        neighbour2 = (y - 1, x - 1)
+    
+    return neighbour1, neighbour2
