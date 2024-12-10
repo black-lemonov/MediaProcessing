c5b7965462bc225cf88176222d8e19e7de776629
black-lemonov <egor.porf1@mail.ru>
2024-12-10 15:59:12 +0300

    [лаб 5] рефакторинг

--- model/lab5.py
+++ model/lab5.py
@@ -1,5 +1,3 @@
-from tkinter import filedialog as fd
-
 import cv2
 import numpy as np
 
@@ -12,13 +10,9 @@ def wb_blur(frame: np.ndarray):
 
 def motion_detection() -> None:
     thresh = 32
-    min_area = 10
+    min_area = 20
 
-    src_path = fd.askopenfilename(
-        title='Выберите изображение:',
-        initialdir="/home/egorp/Видео",
-        filetypes=(('', ".mp4"),)
-    )
+    src_path = "/home/egorp/Видео/ЛР5_видео/archive/ЛР4_main_video.mov"
 
     video = cv2.VideoCapture(src_path)
     w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -32,13 +26,7 @@ def motion_detection() -> None:
 
     frame_prev = wb_blur(frame_prev)
 
-    dest_path = fd.asksaveasfilename(
-        title='Сохранить как...',
-        initialdir="/home/egorp/Видео",
-        initialfile='копия_без_звука.mp4',
-        defaultextension='.mp4',
-        confirmoverwrite=True
-    )
+    dest_path = "/home/egorp/Видео/ЛР5_видео/LR5_motion.mp4"
 
     video_writer = cv2.VideoWriter(dest_path, fourcc, fps, (w, h))
 
@@ -49,16 +37,16 @@ def motion_detection() -> None:
 
         frame = wb_blur(frame_raw)
         frame_diff = cv2.absdiff(frame_prev, frame)
-        ret, frame_threshold = cv2.threshold(frame_diff, thresh, 255, cv2.THRESH_BINARY)
+        _, frame_threshold = cv2.threshold(frame_diff, thresh, 255, cv2.THRESH_BINARY)
         contours, hierarchy = cv2.findContours(frame_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        if all([cv2.contourArea(cnt) >= min_area for cnt in contours]):
+        if any([cv2.contourArea(cnt) > min_area for cnt in contours]):
             video_writer.write(frame_raw)
 
-        cv2.imshow("video", frame_threshold)
+        cv2.imshow("original", frame_raw)
         frame_prev = frame
 
-        if cv2.waitKey(1) & 0xFF == 27:
+        if cv2.waitKey(1) == 27:
             cv2.destroyAllWindows()
             break
 
